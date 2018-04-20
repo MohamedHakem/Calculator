2bde0d92983c87076ef21555e409c7511771cca6
Mohammad Sabri <mohsmh0@gmail.com>
2018-04-20 23:11:24 +0200

    Create crew.js

---
+++ crew.js
@@ -0,0 +1,19 @@
+var number = document.getElementById("number").value;
+
+
+var noOfDigits = number.toString().length;
+
+
+for( i=0; i< noOfDigits;  i++ ){
+	
+	function isEven(){
+	    digit=number/Math.pow(10, noOfDigits-1);
+	  if( digit % 2!= 0){
+		console.log(digit + " is not Even , going to work on it !");
+	  }
+	  elseif(i==nOfDigits-1){
+
+	  }
+}
+isEven();
+};
