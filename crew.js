7eea4ff6961c3900183d59da9a99ead83d76946f
Mohammad Sabri <mohsmh0@gmail.com>
2018-04-21 00:56:35 +0200

    Update crew.js

--- crew.js
+++ crew.js
@@ -19,7 +19,7 @@ function iseven(input_number){
 
   	//Sabri Code 
 	for(var i=1; i<= noOfDigits;  i++ ){
-		var digit=Math.round(input_number/Math.pow(10, noOfDigits-i));
+		var digit=Math.floor(input_number/Math.pow(10, noOfDigits-i));
 		if( digit % 2!= 0){
 			console.log(digit + " is not Even , going to work on it !");
 			alert(digit + " is not Even , going to work on it !");
